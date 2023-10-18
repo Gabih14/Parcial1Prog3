c623b8cd806231092ce39f31d0d129d49a257a1c
Gabriel Hernandez <hernandezg.alvarez@gmail.com>
2023-10-18 13:47:45 -0300

    agrega la paginacion

--- src/main/java/com/utn/ParcialHernandez/controllers/PersonaController.java
+++ src/main/java/com/utn/ParcialHernandez/controllers/PersonaController.java
@@ -3,6 +3,7 @@ package com.utn.ParcialHernandez.controllers;
 import com.utn.ParcialHernandez.entities.Persona;
 import com.utn.ParcialHernandez.services.PersonaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,15 @@ public class PersonaController extends BaseControllerImpl<Persona,PersonaService
         try{
 
             return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\":\""+ e.getMessage()+"\"}"));
+        }
+    }
+    @GetMapping("/searchPaged")
+    public ResponseEntity<?> search (@RequestParam String filtro, Pageable pageable){
+        try{
+
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro, pageable));
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\":\""+ e.getMessage()+"\"}"));
         }
